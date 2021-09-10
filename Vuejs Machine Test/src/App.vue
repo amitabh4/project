47b7dcf97907557511989e67ca7ac9f646c1968e
amitabh pandey <44919634+amitabh4@users.noreply.github.com>
2021-09-10 23:44:47 +0530

    Add files via upload

---
+++ Vuejs Machine Test/src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <AddTodo v-on:add-todo="addTodo"/>
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
+  </div>
+  
+  <div id="app">
+        <picture-input @change="onChange()" width="500" height="500" margin="16" accept="image/jpeg,image/png">
+        </picture-input> 
+</template>
+
+<script>
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo'
+import PictureInput from 'vue-picture-input'
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      todos:[
+        {
+          id:1,
+          title:"todo 1",
+          completed:false
+        },
+        {
+          id:2,
+          title:"todo 2",
+          completed:true
+        },
+        {
+          id:3,
+          title:"todo 3",
+          completed:false
+        },
+        {
+          id:4,
+          title:"todo 4",
+          completed:true
+        }
+      ]
+    }
+  },
+  components: {
+    Todos,
+    AddTodo
+  },
+  methods:{
+
+    addTodo(newTodo){
+      this.todos = [...this.todos,newTodo]
+    },
+    deleteTodo(id){
+      this.todos = this.todos.filter((todo) => todo.id !== id)
+    }
+  }
+},
+export default {
+    name: 'app',
+    components: {
+      PictureInput
+    },
+    methods: {
+      onChange () {
+        console.log("Picture changed!")
+      }
+    }
+  };
+
+</script>
---
+++ Vuejs Machine Test/src/components/AddTodo.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="container">
+        <br>
+        <h1 class="text-center">Todos App</h1>
+        <br><br>
+        <form @submit="addTodo">
+        <div class="form-group">
+            <input required type="text" v-model="email" placeholder="email" name="email" id="" class="form-control">
+        </div>
+        <div class="form-group">
+            <input required type="text" v-model="first_name" placeholder=" first_name" name=" first_Name" id="" class="form-control">
+        </div>
+        <div class="form-group">
+            <input required type="text" v-model="Last_name" placeholder="Last_name" name="Last_Name" id="" class="form-control">
+        </div>
+        <div class="form-group">
+            <button class="btn btn-block btn-danger">
+                Add Todo
+            </button>
+        </div>
+        </form>
+    </div>
+</template>
+<script>
+
+export default {
+  name: "AddTodo",
+  data() {
+    return {
+      title: ''
+    }
+  },
+  methods: {
+    addTodo(e) {
+        e.preventDefault()
+      const newTodo = {
+        id:Date.now(),
+        email: this.email,
+        first_name: this. first_name,
+        Last_name: this. Last_name,
+        
+        completed: false
+      }
+      // Send up to parent
+      this.$emit('add-todo', newTodo);
+
+      this.title = '';
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
